e6e5c3d5e6f8f648f87839e7a01def63c35ca3c7
Jerry <grapesmoker@gmail.com>
2020-06-21 00:08:20 -0400

    alpha status

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
     author_email='grapesmoker@gmail.com',
     python_requires='>=3.5',
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
+        'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
