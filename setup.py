9cfa1903e15799dc384f25e69eccec78a8fdb6f5
Jerry <grapesmoker@gmail.com>
2020-06-20 23:59:48 -0400

    it's rst

--- setup.py
+++ setup.py
@@ -40,6 +40,7 @@ setup(
     install_requires=requirements,
     license="MIT license",
     long_description=readme + '\n\n' + history,
+    long_description_content_type="text/rst",
     include_package_data=True,
     keywords='yenta',
     name='yenta',
