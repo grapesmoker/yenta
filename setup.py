fd4a7718051e7a5da0b1e28da60081bdeb3d3963
grapesmoker <grapesmoker@gmail.com>
2022-02-16 14:57:30 -0500

    Revert "Bump version: 0.3.4 → 1.0.0"
    This reverts commit 2032ec7912736bc47b21148d189f7f8af14faa05.

--- setup.py
+++ setup.py
@@ -50,6 +50,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/grapesmoker/yenta',
-    version='1.0.0',
+    version='0.3.4',
     zip_safe=False,
 )
